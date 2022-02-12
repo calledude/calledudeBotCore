0a8438aa05069ae32033e4b28bf7d96980ee6bc1
calledude <22471295+calledude@users.noreply.github.com>
2022-02-12 10:11:02 +0100

    Add tests for UptimeCommand

---
+++ calledudeBotCore.Tests/Commands/UptimeCommandTests.cs
@@ -0,0 +1,49 @@
+﻿using calledudeBot.Chat.Commands;
+using calledudeBot.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace calledudeBotCore.Tests.Commands;
+
+public class UptimeCommandTests
+{
+    private const string _streamUptime = "Stream uptime:";
+    private readonly Mock<IStreamMonitor> _streamMonitor;
+    public UptimeCommandTests()
+    {
+        _streamMonitor = new Mock<IStreamMonitor>();
+    }
+
+    [Fact]
+    public async Task NotStreaming_Returns_StreamerIsntLive()
+    {
+        _streamMonitor.SetupGet(x => x.IsStreaming).Returns(false);
+
+        var target = new UptimeCommand(_streamMonitor.Object);
+        var result = await target.Handle();
+
+        Assert.Equal("Streamer isn't live.", result);
+    }
+
+    [Theory]
+    [InlineData(1, 0, 0, $"{_streamUptime} 1h")]
+    [InlineData(0, 5, 0, $"{_streamUptime} 5m")]
+    [InlineData(0, 0, 10, $"{_streamUptime} 10s")]
+    [InlineData(2, 0, 1, $"{_streamUptime} 2h 1s")]
+    [InlineData(2, 3, 15, $"{_streamUptime} 2h 3m 15s")]
+    [InlineData(2, 3, 0, $"{_streamUptime} 2h 3m")]
+    [InlineData(0, 0, 0, "The stream has just started.")] //Why?
+    public async Task Streaming(int hoursStreamed, int minutesStreamed, int secondsStreamed, string expectedOutput)
+    {
+        _streamMonitor.SetupGet(x => x.IsStreaming).Returns(true);
+        var streamStarted = DateTime.Now.AddHours(-hoursStreamed).AddMinutes(-minutesStreamed).AddSeconds(-secondsStreamed);
+        _streamMonitor.SetupGet(x => x.StreamStarted).Returns(streamStarted);
+
+        var target = new UptimeCommand(_streamMonitor.Object);
+        var result = await target.Handle();
+
+        Assert.Equal(expectedOutput, result);
+    }
+}
