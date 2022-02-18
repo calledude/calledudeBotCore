409640d92441b94a33a54a2d1348675ac5476165
calledude <22471295+calledude@users.noreply.github.com>
2022-02-18 23:39:50 +0100

    Implement OsuUserServiceTests

--- calledudeBotCore.Tests/ObjectMothers/OsuUserObjectMother.cs
+++ calledudeBotCore.Tests/ObjectMothers/OsuUserObjectMother.cs
@@ -1,9 +1,17 @@
 ﻿using calledudeBot.Models;
+using System.Collections.Generic;
 
 namespace calledudeBotCore.Tests.ObjectMothers;
 
 public static class OsuUserObjectMother
 {
-    public static OsuUser CreateOsuUser()
-        => new("calledude", 42069, "100.12", 4141.41f, 99.95f, 1337, null);
+    public static OsuUser CreateOsuUser(
+        string username = "calledude",
+        int rank = 42069,
+        string level = "100.12",
+        float pp = 4141.41f,
+        float accuracy = 99.95f,
+        int countryRank = 1337,
+        List<object> events = null)
+        => new(username, rank, level, pp, accuracy, countryRank, events);
 }
