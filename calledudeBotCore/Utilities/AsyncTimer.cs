1f17636e414ac6d33937cce2109dbc0c2596d5ac
calledude <22471295+calledude@users.noreply.github.com>
2022-03-05 22:54:01 +0100

    Make AsyncTimer idempotent

--- calledudeBotCore/Utilities/AsyncTimer.cs
+++ calledudeBotCore/Utilities/AsyncTimer.cs
@@ -7,45 +7,50 @@ namespace calledudeBot.Utilities;
 
 public interface IAsyncTimer : IDisposable
 {
-    event Func<CancellationToken, Task> Elapsed;
-    int Interval { get; set; }
+	event Func<CancellationToken, Task> Elapsed;
+	int Interval { get; set; }
 
-    Task Start(CancellationToken cancellationToken);
-    void Stop();
+	Task Start(CancellationToken cancellationToken);
+	void Stop();
 }
 
 public sealed class AsyncTimer : IAsyncTimer
 {
-    private readonly CancellationTokenSource _cts;
-    private readonly ILogger<AsyncTimer> _logger;
+	private readonly CancellationTokenSource _cts;
+	private readonly ILogger<AsyncTimer> _logger;
+	private bool _started;
 
-    public event Func<CancellationToken, Task>? Elapsed;
-    public int Interval { get; set; }
+	public event Func<CancellationToken, Task>? Elapsed;
+	public int Interval { get; set; }
 
-    public AsyncTimer(ILogger<AsyncTimer> logger)
-    {
-        _cts = new CancellationTokenSource();
-        _logger = logger;
-    }
+	public AsyncTimer(ILogger<AsyncTimer> logger)
+	{
+		_cts = new CancellationTokenSource();
+		_logger = logger;
+	}
 
-    public async Task Start(CancellationToken cancellationToken)
-    {
-        if (Elapsed is null)
-            throw new InvalidOperationException(nameof(Elapsed) + " is null");
+	public async Task Start(CancellationToken cancellationToken)
+	{
+		if (Elapsed is null)
+			throw new InvalidOperationException(nameof(Elapsed) + " is null");
 
-        var linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cts.Token);
+		if (_started)
+			return;
 
-        _logger.LogTrace("Starting timer.");
-        while (!linkedTokenSource.IsCancellationRequested)
-        {
-            await Elapsed(linkedTokenSource.Token);
-            await Task.Delay(Interval, linkedTokenSource.Token);
-        }
+		_started = true;
+		var linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cts.Token);
 
-        _logger.LogTrace("Stopping timer.");
-    }
+		_logger.LogTrace("Starting timer.");
+		while (!linkedTokenSource.IsCancellationRequested)
+		{
+			await Elapsed(linkedTokenSource.Token);
+			await Task.Delay(Interval, linkedTokenSource.Token);
+		}
 
-    public void Stop() => _cts.Cancel();
+		_logger.LogTrace("Stopping timer.");
+	}
 
-    public void Dispose() => _cts.Dispose();
+	public void Stop() => _cts.Cancel();
+
+	public void Dispose() => _cts.Dispose();
 }
