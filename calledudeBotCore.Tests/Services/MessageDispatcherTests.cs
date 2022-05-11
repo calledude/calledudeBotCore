2b29f1723f10c29dac7a536bd5ac6ee6c7d2ecbd
calledude <22471295+calledude@users.noreply.github.com>
2022-05-11 21:18:04 +0200

    Cleanup

--- calledudeBotCore.Tests/Services/MessageDispatcherTests.cs
+++ calledudeBotCore.Tests/Services/MessageDispatcherTests.cs
@@ -12,82 +12,77 @@ namespace calledudeBotCore.Tests.Services;
 
 public class MessageDispatcherTests
 {
-    private readonly Logger<MessageDispatcher> _logger;
-    private readonly INotification _notification;
-    private readonly Mock<INotificationHandler<INotification>> _throwingHandler;
-    private readonly Mock<INotificationHandler<INotification>> _normalHandler;
+	private readonly Logger<MessageDispatcher> _logger;
+	private readonly INotification _notification;
+	private readonly Mock<INotificationHandler<INotification>> _throwingHandler;
+	private readonly Mock<INotificationHandler<INotification>> _normalHandler;
 
-    public MessageDispatcherTests()
-    {
-        _logger = new Logger<MessageDispatcher>(NullLoggerFactory.Instance);
-        _notification = new Mock<INotification>().Object;
+	public MessageDispatcherTests()
+	{
+		_logger = new Logger<MessageDispatcher>(NullLoggerFactory.Instance);
+		_notification = new Mock<INotification>().Object;
 
-        _throwingHandler = new Mock<INotificationHandler<INotification>>();
+		_throwingHandler = new Mock<INotificationHandler<INotification>>();
 
-        _throwingHandler
-            .Setup(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()))
-            .ThrowsAsync(new InvalidOperationException())
-            .Verifiable();
+		_throwingHandler
+			.Setup(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()))
+			.ThrowsAsync(new InvalidOperationException());
 
-        _normalHandler = new Mock<INotificationHandler<INotification>>();
+		_normalHandler = new Mock<INotificationHandler<INotification>>();
+	}
 
-        _normalHandler
-            .Setup(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()))
-            .Verifiable();
-    }
+	[Fact]
+	public async Task Publish_Throws_AllActionsAreExecuted()
+	{
+		var notificationHandlers = new[]
+		{
+			_throwingHandler.Object,
+			_normalHandler.Object
+		};
 
-    [Fact]
-    public async Task Publish_Throws_AllActionsAreExecuted()
-    {
-        var notificationHandlers = new[]
-        {
-            _throwingHandler.Object,
-            _normalHandler.Object
-        };
+		var mediator = new CustomMediator(_ => notificationHandlers);
 
-        var mediator = new CustomMediator(_ => notificationHandlers);
+		var dispatcher = new MessageDispatcher(_logger, mediator);
 
-        var dispatcher = new MessageDispatcher(_logger, mediator);
+		await dispatcher.PublishAsync(_notification);
 
-        await dispatcher.PublishAsync(_notification);
+		_throwingHandler.Verify(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()), Times.Once);
+		_normalHandler.Verify(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()), Times.Once);
+	}
 
-        _throwingHandler.Verify(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()), Times.Once);
-        _normalHandler.Verify(x => x.Handle(It.IsAny<INotification>(), It.IsAny<CancellationToken>()), Times.Once);
-    }
+	[Fact]
+	public async Task Publish_NeverThrows()
+	{
+		var notificationHandlers = new[]
+		{
+			_throwingHandler.Object
+		};
 
-    [Fact]
-    public async Task Publish_NeverThrows()
-    {
-        var notificationHandlers = new[]
-        {
-            _throwingHandler.Object
-        };
+		var mediator = new CustomMediator(_ => notificationHandlers);
 
-        var mediator = new CustomMediator(_ => notificationHandlers);
+		var dispatcher = new MessageDispatcher(_logger, mediator);
 
-        var dispatcher = new MessageDispatcher(_logger, mediator);
+		var publishTask = dispatcher.PublishAsync(_notification);
+		await publishTask;
 
-        var publishTask = dispatcher.PublishAsync(_notification);
-        await publishTask;
+		Assert.False(publishTask.IsFaulted);
+	}
 
-        Assert.False(publishTask.IsFaulted);
-    }
+	[Fact]
+	public async Task Publish_WithNormalHandler_Works()
+	{
+		var notificationHandlers = new[]
+		{
+			_normalHandler.Object
+		};
 
-    [Fact]
-    public async Task Publish_WithNormalHandler_Works()
-    {
-        var notificationHandlers = new[]
-        {
-            _normalHandler.Object
-        };
+		var mediator = new CustomMediator(_ => notificationHandlers);
 
-        var mediator = new CustomMediator(_ => notificationHandlers);
+		var dispatcher = new MessageDispatcher(_logger, mediator);
 
-        var dispatcher = new MessageDispatcher(_logger, mediator);
+		var publishTask = dispatcher.PublishAsync(_notification);
+		await publishTask;
 
-        var publishTask = dispatcher.PublishAsync(_notification);
-        await publishTask;
-
-        Assert.False(publishTask.IsFaulted);
-    }
+		Assert.False(publishTask.IsFaulted);
+	}
 }
