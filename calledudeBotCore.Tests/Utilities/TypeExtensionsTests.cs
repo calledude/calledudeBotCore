854816fdc6adb77e52c93c3362e6957031695c75
calledude <22471295+calledude@users.noreply.github.com>
2022-05-11 21:47:58 +0200

    Implement TypeExtensionsTests

---
+++ calledudeBotCore.Tests/Utilities/TypeExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using calledudeBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xunit;
+
+namespace calledudeBotCore.Tests.Utilities;
+public class TypeExtensionsTests
+{
+	[Theory]
+	[InlineData(typeof(string), "string")]
+	[InlineData(typeof(object), "object")]
+	[InlineData(typeof(bool), "bool")]
+	[InlineData(typeof(byte), "byte")]
+	[InlineData(typeof(char), "char")]
+	[InlineData(typeof(decimal), "decimal")]
+	[InlineData(typeof(double), "double")]
+	[InlineData(typeof(short), "short")]
+	[InlineData(typeof(int), "int")]
+	[InlineData(typeof(long), "long")]
+	[InlineData(typeof(sbyte), "sbyte")]
+	[InlineData(typeof(float), "float")]
+	[InlineData(typeof(ushort), "ushort")]
+	[InlineData(typeof(uint), "uint")]
+	[InlineData(typeof(ulong), "ulong")]
+	[InlineData(typeof(void), "void")]
+	public void SimpleType(Type t, string expectedFriendlyName) => Assert.Equal(expectedFriendlyName, t.GetFriendlyName());
+
+	[Theory]
+	[InlineData(typeof(bool*), "bool*")]
+	[InlineData(typeof(byte*), "byte*")]
+	[InlineData(typeof(char*), "char*")]
+	[InlineData(typeof(decimal*), "decimal*")]
+	[InlineData(typeof(double*), "double*")]
+	[InlineData(typeof(short*), "short*")]
+	[InlineData(typeof(int*), "int*")]
+	[InlineData(typeof(long*), "long*")]
+	[InlineData(typeof(sbyte*), "sbyte*")]
+	[InlineData(typeof(float*), "float*")]
+	[InlineData(typeof(ushort*), "ushort*")]
+	[InlineData(typeof(uint*), "uint*")]
+	[InlineData(typeof(ulong*), "ulong*")]
+	[InlineData(typeof(void*), "void*")]
+	public void PointerType(Type t, string expectedFriendlyName) => Assert.Equal(expectedFriendlyName, t.GetFriendlyName());
+
+	[Theory]
+	[InlineData(typeof(string[]), "string[]")]
+	[InlineData(typeof(object[]), "object[]")]
+	[InlineData(typeof(bool[]), "bool[]")]
+	[InlineData(typeof(byte[]), "byte[]")]
+	[InlineData(typeof(char[]), "char[]")]
+	[InlineData(typeof(decimal[]), "decimal[]")]
+	[InlineData(typeof(double[]), "double[]")]
+	[InlineData(typeof(short[]), "short[]")]
+	[InlineData(typeof(int[]), "int[]")]
+	[InlineData(typeof(long[]), "long[]")]
+	[InlineData(typeof(sbyte[]), "sbyte[]")]
+	[InlineData(typeof(float[]), "float[]")]
+	[InlineData(typeof(ushort[]), "ushort[]")]
+	[InlineData(typeof(uint[]), "uint[]")]
+	[InlineData(typeof(ulong[]), "ulong[]")]
+	public void ArrayType(Type t, string expectedFriendlyName) => Assert.Equal(expectedFriendlyName, t.GetFriendlyName());
+
+	[Theory]
+	[InlineData(typeof(List<int*[]>), "List<int*[]>")]
+	[InlineData(typeof(List<List<int>>), "List<List<int>>")]
+	[InlineData(typeof(Dictionary<int, string>), "Dictionary<int, string>")]
+	[InlineData(typeof(Dictionary<int, List<KeyValuePair<string, HttpClient>>>), "Dictionary<int, List<KeyValuePair<string, HttpClient>>>")]
+	public void GenericType(Type t, string expectedFriendlyName) => Assert.Equal(expectedFriendlyName, t.GetFriendlyName());
+}
